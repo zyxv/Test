773e92e4613d9741dd7f893b0060d83844a856dc
Cody Weaver <codyleeweaver@gmail.com>
2017-08-19 18:55:04 -0700

    Switched to Obj-C

---
+++ TestCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestCode
+//
+//  Created by Cody Weaver on 8/19/17.
+//  Copyright © 2017 BlueBerry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
